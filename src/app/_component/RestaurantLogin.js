63adab6c9840546cfc2146f4e4a181f10a6541d0
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2025-12-14 13:37:22 +0530

    login page started

--- src/app/_component/RestaurantLogin.js
+++ src/app/_component/RestaurantLogin.js
@@ -1,23 +1,41 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Key, PersonFill } from "react-bootstrap-icons";
 
 export default function RestaurantLogin() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+
+  const handleloginsubmit=(e)=>{
+    e.preventDefault();
+    if(!email || !password){
+      setError(true);
+    }else{
+      setError(false);
+    }
+    console.log(email,password);
+  }
+
   return (
     <div className="d-flex justify-content-center mt-2">
-      <div className="p-4 shadow-sm rounded col-md-5">
+      <div className="p-4  col-md-5">
         <h3 className="text-center mb-4">Login Page</h3>
 
-        <form>
+        <form onSubmit={handleloginsubmit}>
           <div className="input-group mb-3">
             <span className="input-group-text">
               <PersonFill />
             </span>
             <input
-              type="text"
+              type="email"
               className="form-control"
               placeholder="Enter Email"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
             />
+            {error && !email && <span className="text-danger">Enter valid email address</span>}
           </div>
 
           <div className="input-group mb-3">
@@ -28,7 +46,11 @@ export default function RestaurantLogin() {
               type="password"
               className="form-control"
               placeholder="Enter Password"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
             />
+                        {error && !password && <span className="text-danger">Enter valid password</span>}
+
           </div>
 
           <div className="text-center mt-3">
